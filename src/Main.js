d184d370578dadb28db20a42e43969a8783fc783
anmoln <sirkinggreat@gmail.com>
2023-10-05 03:58:15 +0530

    initial

---
+++ src/Main.js
@@ -0,0 +1,21 @@
+// Main.js (using React Bootstrap)
+
+import React from 'react';
+import { Container, Row} from 'react-bootstrap';
+import Header from './components/Header/Header'; // Import your Header component
+import AddAppointment from './components/Appointment/AddAppointment';
+
+function Main() {
+  return (
+    <Container fluid>
+      <Header />
+      
+          <AddAppointment />
+    
+
+   
+    </Container>
+  );
+}
+
+export default Main;
